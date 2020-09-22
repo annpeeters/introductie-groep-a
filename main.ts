df37f4e7a3c38a4430a88d628325a6e2ff90a531
annpeeters <43540270+annpeeters@users.noreply.github.com>
2020-09-22 12:16:13 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,18 @@
+basic.showLeds(`
+    . . . . .
+    . # # . .
+    . # # . .
+    . # # . .
+    . . . . .
+    `)
 basic.forever(function () {
-	
+    if (input.lightLevel() < 100) {
+        basic.showLeds(`
+            . . . . #
+            . . . . #
+            . . . . #
+            . . . . #
+            . . . . #
+            `)
+    }
 })
